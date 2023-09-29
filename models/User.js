db5070fe656fe64ebcce979fc0a7438cf5225125
Daniel Rangel <000.drf@gmail.com>
2023-09-28 19:14:16 -0700

    User schema setup.

---
+++ models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export default new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
